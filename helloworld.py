bf2f06a566a9b15116b83461f6f33c45bbf5c974
brsmythe <33708975+brsmythe@users.noreply.github.com>
2019-09-09 15:43:31 -0400

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,10 @@
+from random import randint
+print "Brian Smythe"
+x = int(randint(1,101))
+print x
+y = int(randint(1,101))
+print y
+result = x + y
+print ("Sum = " + str(result))
+result = result / 2
+print ("Average = " + str(result))
